6a9d605fbf90aa64297888fa469401dec7e036f3
mintaeKim <peterzzo@naver.com>
2024-01-16 21:06:31 +0900

    240116 useeffect end / cleanup function

--- react-app-MovieWeb/src/App.js
+++ react-app-MovieWeb/src/App.js
@@ -3,12 +3,28 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 import styled from "./Button.module.css";
 
+function Hello(){
+  function created(){
+    console.log("I'm created ><");
+    return destroyed;
+  }
+  function destroyed(){
+    console.log("destroyed ㅠㅠ");
+  }
+  useEffect(created, []);
+  return(
+    <h1>Hello</h1>
+  );
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
 
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClickShow = () => setShowing((prev) => !prev);
 
   useEffect(() => {
     console.log("i run only once");
@@ -22,9 +38,11 @@ function App() {
 
   return (
     <div>
+      {showing ? <Hello/> : null}
       <input value={keyword} onChange={onChange} type="text" placeholder="search here..."></input>
       <h1>{counter}</h1>
-      <button className={styled.btn} onClick={onClick}>click me</button>
+      <button className={styled.btn} onClick={onClick}>click me</button><br></br>
+      <button onClick={onClickShow}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
