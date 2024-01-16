bf19e30e5f222f2e944a71260a14ad759ac1807d
mintaeKim <peterzzo@naver.com>
2024-01-16 20:08:20 +0900

    240116 useeffect rendering one time practice

--- react-app-MovieWeb/src/App.js
+++ react-app-MovieWeb/src/App.js
@@ -1,11 +1,20 @@
 import Button from "./Button.js";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+import styled from "./Button.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const irunonetime = () =>{
+    console.log("i run only once");
+  }
+  useEffect(irunonetime, []);
   return (
     <div>
-      <h1 className={styles.title}>hello!!</h1>
-      <Button text={"click me"}/>
+      <h1>{counter}</h1>
+      <button className={styled.btn} onClick={onClick}>click me</button>
     </div>
   );
 }
