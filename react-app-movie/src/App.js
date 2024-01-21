d318943e00b0a063bf86d6cef284e8fb742b2591
mintaeKim <peterzzo@naver.com>
2024-01-21 20:27:08 +0900

    240121 react movie service code 01

--- react-app-movie/src/App.js
+++ react-app-movie/src/App.js
@@ -2,18 +2,32 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
+function App(){
+  const router = createBrowserRouter([
+    {
+    path: "/",
+    element: <Home />,
+    },
+    {
+    path: "/detail/:id",
+    element: <Detail />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+/*
 const App = () => {
 const router = createBrowserRouter([
-{
-path: "/",
-element: <Home />,
-},
-{
-path: "/detail",
-element: <Detail />,
-},
+  {
+  path: "/",
+  element: <Home />,
+  },
+  {
+  path: "/detail",
+  element: <Detail />,
+  },
 ]);
 return <RouterProvider router={router} />;
 };
-
+*/
 export default App;
